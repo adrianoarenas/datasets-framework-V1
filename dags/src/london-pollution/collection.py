9ed0afa51903f0ba4c3a4efcad68131978d564b3
Adriano Arenas <adrianoarenas@gmail.com>
2023-01-09 22:37:01 +0000

    uncommented code

--- dags/src/london-pollution/collection.py
+++ dags/src/london-pollution/collection.py
@@ -16,13 +16,13 @@ bucket_name = 'adriano-portfolio-data-lake-euwest2'
 
 print(secrets)
 
-# #Calling API
-# api_link = 'https://api.tfl.gov.uk/AirQuality/'
-# response = requests.get(api_link)
-# json_text = response.json()
+#Calling API
+api_link = 'https://api.tfl.gov.uk/AirQuality/'
+response = requests.get(api_link)
+json_text = response.json()
 
-# #Dumping response to json
-# json_string = json.dumps(json_text)
+#Dumping response to json
+json_string = json.dumps(json_text)
 
-# #Loading json to S3
-# s3_load('london-pollution', json_string, bucket_name, secrets)
+#Loading json to S3
+s3_load('london-pollution', json_string, bucket_name, secrets)
